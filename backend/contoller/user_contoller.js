cc3bd80b3cf1b0140e4b8d09c44f49e55fdbb4f7
kashisharora1202 <kashish.arora1202@gamil.com>
2026-06-29 16:49:20 +0530

    email added

--- backend/contoller/user_contoller.js
+++ backend/contoller/user_contoller.js
@@ -6,13 +6,14 @@ const bcrypt = require("bcrypt")
 async function createuser_controller(req,res){
     try {
         
-        const {username,password}=req.body
+        const {username,password,email}=req.body
 
         const hashpassword = await bcrypt.hash(password, 10)
 
         const user = await user_schema.create({
             username:username,
-            password: hashpassword
+            password: hashpassword,
+            email:email
         })
 
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
