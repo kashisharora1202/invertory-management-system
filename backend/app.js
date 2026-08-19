cd2490c34621f29aa5bcb691de348ca01979072c
kashisharora1202 <kashish.arora1202@gamil.com>
2026-08-19 15:53:20 +0530

    backend .env update

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,13 @@ const cookieparser = require("cookie-parser")
 const login = require("./routes/login") 
 const addProduct = require("./routes/add_product")
 const show_all_products = require("./routes/show_all_products")
+const cors = require("cors")
 
 const app = express()
 connect()
 app.use(express.json())
 app.use(cookieparser())
+app.use(cors())
 
 
 app.use("/create", createuser)
@@ -18,6 +20,8 @@ app.use("/user",login)
 app.use("/add",addProduct)
 app.use("/show",show_all_products)
 
-app.listen(4000,()=>{
-    console.log("server is running port number 4000")
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`server is running port number ${PORT}`)
 })
