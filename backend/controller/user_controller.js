f61b68f6a7925ddbeaa7940298422938102b3824
kashisharora1202 <kashish.arora1202@gamil.com>
2026-09-02 09:59:35 +0530

    minimum update

--- backend/controller/user_controller.js
+++ backend/controller/user_controller.js
@@ -18,7 +18,7 @@ async function createuser_controller(req,res){
         })
 
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-        res.cookie("token",token {
+        res.cookie("token",token ,{
             httpOnly: true,
             secure: true
         })
