fadeccf61d4de0c4e7dc700b5fd1de5d6b7bca5a
kashisharora1202 <kashish.arora1202@gamil.com>
2026-09-06 18:07:30 +0530

    edit login page

--- backend/controller/user_login_controller.js
+++ backend/controller/user_login_controller.js
@@ -30,13 +30,13 @@ async function login(req,res) {
         
          const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
 
-         // res.cookie("token",token ,{
-         //    httpOnly: true,
-         //    secure: true,
-         //    sameSite: "none"
-         // })
+         res.cookie("token",token ,{
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+         })
 
-         res.cookie("token",token)
+         // res.cookie("token",token)
          
 
          return res.status(200).json({message:"login successfully",user:user})
