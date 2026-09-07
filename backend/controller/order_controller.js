8cd946e9065e275f4acbc7abee69e1965464fa1d
kashisharora1202 <kashish.arora1202@gamil.com>
2026-09-07 18:36:17 +0530

    order controller api bug fix

--- backend/controller/order_controller.js
+++ backend/controller/order_controller.js
@@ -1,10 +1,25 @@
 const express = require("express")
 const orderSchema = require("../schema/order_schema")
+const productSchema = require("../schema/product_schema")
 
 async function order(req,res) {
     try {
             const {product , image , price , category , coustmorname , phoneno , quantity , address , city , pincode , selerid } = req.body
 
+            const productowner = await productSchema.findOne({
+                $and:[
+                   {userid : selerid},
+                    {product:req.body.product}
+                
+                ] 
+            })
+            if(productowner.stock==0){
+                return res.status(201).json({message:"out of stock"})
+            }
+
+            if(quantity>productowner.stock ){
+                return res.status(201).json({message:"We have less stock of this product"})
+            }
 
             const orders = await orderSchema.create({
               product,
@@ -19,10 +34,25 @@ async function order(req,res) {
               pincode,
               selerid ,
               coustmorid : req.user.id
-            })
+            })      
 
-            res.status(201).json({message:"congratulations your order has beed placed" , order:orders})
+                
+            const updatestock = productowner.stock - quantity 
+            console.log(updatestock)
+            
+            const updated = await productSchema.findOneAndUpdate({
+                $and:[
+                    {userid : selerid},
+                    {product : req.body.product}
+                ]},
+                {$set:{stock : updatestock}},
+                {
+                    new:true
+                }
+                
+            )
 
+            res.status(201).json({message:"congratulations your order has beed placed" , order:orders , updated:updated})
 
     } catch (error) {
         res.status(500).json({message:"server not responce ", error:error.message})
